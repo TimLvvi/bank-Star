1378c1615872b666846546d0d9066f5c41d0fc1c
TimLvv <lavrentev.timur@mail.ru>
2026-01-15 21:29:02 +0300

    initial commit

---
+++ src/main/java/org/skypro/bank_Star/BankStarApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.bank_Star;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankStarApplication.class, args);
+	}
+
+}
